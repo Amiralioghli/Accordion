b9405c199f3259b9c9a7cb32b2c8cdff4e883441
Abdul Amir Alioghli <104595848+Amiralioghli@users.noreply.github.com>
2025-07-22 11:47:19 -0400

    Final update

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const accordions = document.querySelectorAll('.content-container');
 
+
 accordions.forEach(container => {
   container.querySelector('.question').addEventListener('click', function () {
     container.classList.toggle('active');
   });
 });
+
+
+
